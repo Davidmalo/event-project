77721e2ac23e5e03afd7c4fdd974a5ce21f7f74b
ErezM11 <erezmantzoor11@gmail.com>
2025-03-28 16:39:47 +0300

    add

--- project/src/Pages/MainPage.jsx
+++ project/src/Pages/MainPage.jsx
@@ -8,10 +8,20 @@ import add2 from '../assets/ErezImages/add2.png'
 import add3 from '../assets/ErezImages/add3.png'
 import add4 from '../assets/ErezImages/add4.png'
 import add5 from '../assets/ErezImages/add5.png'
+import img4 from '../assets/ErezImages/img4.png'
 
 export default function MainPage() {
     return (
         <div className='mainPage'>
+            <div className='header d-flex justify-content-between'>
+                <div className='left-header'>
+                    <span className='d-flex'><h2>Event</h2> <h2 className='purple'>Hive</h2> </span>
+                </div>
+                <div className='right-header d-flex'>
+                    <button>Login</button>
+                    <button className='SignUp'>SignUp</button>
+                </div>
+            </div>
             <div className='topPage bg-danger'>
                 <h2>MADE FOR THOSE WHO DO</h2>
                 <div className='search-bar d-flex justify-content-between'>
@@ -34,13 +44,13 @@ export default function MainPage() {
                         </select>
                     </div>
                     <div className='search-icon'>
-                        <p>Search</p>
+                        <img src="https://icon-library.com/images/white-search-icon-transparent-background/white-search-icon-transparent-background-1.jpg" alt="" />
                     </div>
                 </div>
             </div>
 
             <div className='Upcoming d-flex justify-content-between'>
-                <div className='Upcoming-title d-flex'><h4>Upcoming</h4> <h4 className='purple'>Events</h4></div>
+                <div className='Upcoming-title d-flex'><h4>Upcoming</h4><h4 className='purple'>Events</h4></div>
                 <div className='d-flex'>
                     <select name="" id=""><option value="">Weekends</option></select>
                     <select name="" id=""><option value="">Event type</option></select>
@@ -109,70 +119,11 @@ export default function MainPage() {
                         <p className='gray'>ONLINE EVENT - Attend anywhere</p>
                     </div>
                 </div>
-                <div className='oneCard'>
-                    <div className='card-img'>
-                        <img src={img2} alt="" />
-                    </div>
-                    <div className='card-content'>
-                        <p className='card-title'><b>BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</b></p>
-                        <p className='purple'>Saturdat, March 18, 9.30PM</p>
-                        <p className='gray'>ONLINE EVENT - Attend anywhere</p>
-                    </div>
-                </div>
-                <div className='oneCard'>
-                    <div className='card-img'>
-                        <img src={img2} alt="" />
-                    </div>
-                    <div className='card-content'>
-                        <p className='card-title'><b>BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</b></p>
-                        <p className='purple'>Saturdat, March 18, 9.30PM</p>
-                        <p className='gray'>ONLINE EVENT - Attend anywhere</p>
-                    </div>
-                </div>
-                <div className='oneCard'>
-                    <div className='card-img'>
-                        <img src={img2} alt="" />
-                    </div>
-                    <div className='card-content'>
-                        <p className='card-title'><b>BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</b></p>
-                        <p className='purple'>Saturdat, March 18, 9.30PM</p>
-                        <p className='gray'>ONLINE EVENT - Attend anywhere</p>
-                    </div>
-                </div>
-                <div className='oneCard'>
-                    <div className='card-img'>
-                        <img src={img2} alt="" />
-                    </div>
-                    <div className='card-content'>
-                        <p className='card-title'><b>BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</b></p>
-                        <p className='purple'>Saturdat, March 18, 9.30PM</p>
-                        <p className='gray'>ONLINE EVENT - Attend anywhere</p>
-                    </div>
-                </div>
-                <div className='oneCard'>
-                    <div className='card-img'>
-                        <img src={img2} alt="" />
-                    </div>
-                    <div className='card-content'>
-                        <p className='card-title'><b>BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</b></p>
-                        <p className='purple'>Saturdat, March 18, 9.30PM</p>
-                        <p className='gray'>ONLINE EVENT - Attend anywhere</p>
-                    </div>
-                </div>
-                <div className='oneCard'>
-                    <div className='card-img'>
-                        <img src={img2} alt="" />
-                    </div>
-                    <div className='card-content'>
-                        <p className='card-title'><b>BestSelller Book Bootcamp -write, Market & Publish Your Book -Lucknow</b></p>
-                        <p className='purple'>Saturdat, March 18, 9.30PM</p>
-                        <p className='gray'>ONLINE EVENT - Attend anywhere</p>
-                    </div>
-                </div>
+  
             </div>
 
             <div className='learn-more'>
-                <button>Learn more</button>
+                <button>Learn more...</button>
             </div>
             <div className='CreateEvents  d-flex'>
                 <div className='CreateEvents-img'>
@@ -194,6 +145,61 @@ export default function MainPage() {
                     <img src={add4} alt="" />
                     <img src={add5} alt="" />
                 </div>
+            </div>
+            <div className='Treding-colleges'>
+                <div className='Treding-title'>
+                    <div className='d-flex'><h2>Trending</h2> <h2 className='purple'>colleges</h2></div>
+                </div>
+                <div className='Treding-cards d-flex justify-content-center'>
+                    <div className='Treding-one-card'>
+                        <div>
+                            <img src={img4} alt="" />
+                            <div className='rating d-flex justify-content-around'>
+                                <p className='star'>4.8</p>
+                                <p className='excl'>EXCLUSIVE</p>
+                            </div>
+                        </div>
+                        <h2>Harvard University</h2>
+                        <div className='card-but d-flex justify-content-between'>
+                            <p>Cambridge, Massachusetts, UK</p>
+                            <button>•••</button>
+                        </div>
+                        
+                    </div>
+                    <div className='Treding-one-card'>
+                        <div>
+                            <img src={img4} alt="" />
+                            <div className='rating d-flex justify-content-around'>
+                                <p className='star'>4.8</p>
+                                <p className='excl'>EXCLUSIVE</p>
+                            </div>
+                        </div>
+                        <h2>Harvard University</h2>
+                        <div className='card-but d-flex justify-content-between'>
+                            <p>Cambridge, Massachusetts, UK</p>
+                            <button>•••</button>
+                        </div>
+                        
+                    </div>
+                    <div className='Treding-one-card'>
+                        <div>
+                            <img src={img4} alt="" />
+                            <div className='rating d-flex justify-content-around'>
+                                <p className='star'>4.8</p>
+                                <p className='excl'>EXCLUSIVE</p>
+                            </div>
+                        </div>
+                        <h2>Harvard University</h2>
+                        <div className='card-but d-flex justify-content-between'>
+                            <p>Cambridge, Massachusetts, UK</p>
+                            <button>•••</button>
+                        </div>
+                    </div>
+                    
+                </div>
+                <div className='learn-more'>
+                <button>Learn more...</button>
+            </div>
             </div>
         </div>
     )
